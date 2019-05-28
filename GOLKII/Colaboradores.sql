5e639f9e05a11a31530dd3b45d3a3e7c01e48304
Sergio Lopez <serlopsal_199@yahoo.es>
2019-05-28 10:59:33 -0600

    Create Colaboradores.sql

---
+++ GOLKII/Colaboradores.sql
@@ -0,0 +1,9 @@
+SELECT * FROM GOLKII.DBO.Cargo
+
+insert into GOLKII.DBO.Colaboradores 
+(IdCargo,IdBiometrico,Nombre,Cedula,Correo,Extension,Estado)
+VALUES
+(1,64,'Juan Torres','0010412860001N','juan.torrez@golkiibpo.com',214,1),
+(1,63,'Claudia Guevara','0012402810020P','claudia.guevara@golkiibpo.com',203,1)
+
+select * from Golkii.dbo.Colaboradores
